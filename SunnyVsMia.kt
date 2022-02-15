437c9a2136ef4f3b503c30e42750f68e694d7f37
Gnanesh RG <31344456+gnanesh023@users.noreply.github.com>
2022-02-15 17:45:29 +0530

    Create SunnyVsMia.kt

---
+++ SunnyVsMia.kt
@@ -0,0 +1,14 @@
+fun main() {
+var sunny:Int= 38
+var mia :Int = 40
+   if (sunny == mia){
+       print("Both are Eqal")
+   }
+   else if (sunny >= mia){
+       print("Sunny is Worthy")
+   }
+   else{
+       print ("Mia is Worthy")
+   }
+
+}
